c3ec4a382d9c2da8dcce0cb0a0ab7a98d4c666f4
KyooKimiri <aizenkumukiri@gmail.com>
2024-10-31 13:12:01 +0800

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-        var menu = document.getElementById("menuItems");
-        if (menu.classList.contains("open")) {
-            menu.classList.remove("open");
-        } else {
-            menu.classList.add("open");
-        }
-    }
